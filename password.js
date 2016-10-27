8fec44da6edbec71845db66c03758a1830da568e
Silvana Goberdhan-Vigle <silvanagoberdhanvigle@gmail.com>
2016-10-26 23:47:07 -0400

    Done.

--- password.js
+++ password.js
@@ -1,1 +1,9 @@
-// code solution here
+var password = ""
+process.argv.shift()
+process.argv.shift()
+process.argv.forEach(function work(length, number){
+  for (let i = 0;i < length;i++){
+    password += "a"
+  }
+  console.log(password)
+})
